82f1f4cd342310c9b01073686ccbc33d490652ba
zhangjy <zjy191024@163.com>
2023-10-07 16:23:19 +0800

    国家开放大学自动学习脚本

---
+++ guokaiStudy.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         国家开放大学自动学习(新平台)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  国家开放大学自动学习
+// @author       ZhangJinYuan
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license          GPL-3.0
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	 console.log('加载了...');
+})();
