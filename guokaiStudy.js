00b2737c3bba60f6ed8d4c0221556b0b412b1e5a
zhangjy <zjy191024@163.com>
2023-10-08 09:26:09 +0800

    测试

--- guokaiStudy.js
+++ guokaiStudy.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         国家开放大学自动学习(新平台)
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.1
 // @description  国家开放大学自动学习
 // @author       ZhangJinYuan
-// @match        http://*/*
+// @match        *://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license          GPL-3.0
@@ -13,5 +13,5 @@
 (function () {
 	'use strict';
 
-	 console.log('加载了...111');
+	 console.log('测试了');
 })();
